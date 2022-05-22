7f59ee6a71e212dd326c6f893c0188465105b9bf
Owen P. F. Li <pfowenli@gmail.com>
2022-05-22 23:50:53 +0800

    add documentation comments to auth-jwt

--- auth-jwt/json-web-token-generator.js
+++ auth-jwt/json-web-token-generator.js
@@ -1,17 +1,36 @@
 const jwt = require('jsonwebtoken')
 
 class JsonWebTokenGenerator {
+    /**
+     * @constructor
+     * @param {string} privateKey Private key
+     * @param {string} publicKey Public key
+     * @param {object} options Default options
+     */
     constructor(privateKey = '', publicKey = '', options = {}) {
         this.privateKey = privateKey
         this.publicKey = publicKey
         this.options = options
     }
 
+    /**
+     * Sign token
+     * @param {object} payload Payload
+     * @param {object} options
+     * @returns {string}
+     */
     signToken(payload = {}, options = {}) {
         const signOptions = Object.assign({}, options, this.options)
         return jwt.sign(payload, this.privateKey, signOptions)
     }
 
+    /**
+     * Refresh token
+     * @param {string} token Token
+     * @param {object} verifyOptions Verification options
+     * @param {string} jwtid JWT id
+     * @returns {string}
+     */
     refreshToken(token = '', verifyOptions = {}, jwtid = '') {
         const payload = jwt.verify(token, this.publicKey, verifyOptions)
         delete payload.iat
